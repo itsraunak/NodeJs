db804899bac75cd570846aafeb30e260aa7164d8
Raunak Kumar Sah <itisraunak@gmail.com>
2023-09-23 18:56:05 +0530

    Added Ejs and Rendered Dynamic File
    Signed-off-by: Raunak Kumar Sah <itisraunak@gmail.com>

---
+++ 5. Model-View-Controller - I/5.3 Product Listing with Data Fetching/src/controllers/product.controller.js
@@ -0,0 +1,12 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+
+import ProductModel from "../models/product.model.js";
+const productModel = new ProductModel();
+
+export default class ProductController {
+    getProducts = (req, res) => {
+        let products = productModel.fetchProducts();
+        res.render("product", { products });
+    };
+}
