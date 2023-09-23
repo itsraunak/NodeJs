c0b3559ac8815dd6b1a8df2ff49f228104804957
Raunak Kumar Sah <itisraunak@gmail.com>
2023-09-23 19:18:34 +0530

    Created A project Where Data is Fetched using Axios and rendered using EJS with the MVC Structure
    Signed-off-by: Raunak Kumar Sah <itisraunak@gmail.com>

---
+++ 5. Model-View-Controller - I/5.4 User Data Rendering/src/controllers/users.controller.js
@@ -0,0 +1,10 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+import { userModel } from "../models/users.model.js";
+
+export const userController = async (req, res) => {
+    const users = await userModel();
+    // Write your code here
+    // console.log(userData);
+    res.render("index", { users });
+};
